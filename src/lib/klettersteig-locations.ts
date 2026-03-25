73662d75273ae28021110306afcd1144ac6b572f
d.fass <d.fass@rolling-tons.com>
2026-03-25 21:16:40 +0100

    fix: koordinaten von hohen wand richtig gesetzt

--- src/lib/klettersteig-locations.ts
+++ src/lib/klettersteig-locations.ts
@@ -6,9 +6,9 @@ export interface GebirgeLocation {
 }
 
 export const GEBIRGE_LOCATIONS: GebirgeLocation[] = [
-  { id: "hohe-wand", name: "Hohe Wand", center: [47.829, 16.039], zoom: 15 },
-  { id: "rax", name: "Rax", center: [47.685, 15.768], zoom: 14 },
-  { id: "schneeberg", name: "Schneeberg", center: [47.767, 15.808], zoom: 14 },
+  { id: "hohe-wand", name: "Hohe Wand", center: [47.822, 16.046], zoom: 13 },
+  { id: "rax", name: "Rax", center: [47.715, 15.735], zoom: 13 },
+  { id: "schneeberg", name: "Schneeberg", center: [47.755, 15.793], zoom: 13 },
 ];
 
 export const GEBIRGE_MAP: Record<string, GebirgeLocation> = Object.fromEntries(
